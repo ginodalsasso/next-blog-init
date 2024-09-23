b6e381fbbb6da5738952357619b5214c855dba07
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-09-23 22:56:49 +0200

    first commit

---
+++ app/article/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const page = () => {
+    return <div>
+        <h1 className="">Article</h1>
+        <p>Article content</p>
+    </div>;
+};
+
+export default page;
---
+++ lib/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+
+// Déclarer une variable globale pour stocker l'instance de PrismaClient
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
+// Créer une instance de PrismaClient
+export const db = globalThis.prisma || new PrismaClient();
+
+// Si nous sommes en mode développement, nous assignons l'instance de Prisma à la variable globale
+if (process.env.NODE_ENV !== 'production') {
+    globalThis.prisma = db;
+}
