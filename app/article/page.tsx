594a2f6a499e5f1a8b0f3993a1da0115cf8442f9
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-09-24 11:27:43 +0200

    fist request to prisma and article list

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,8 +1,23 @@
+import { db } from "@/lib/db";
 import React from "react";
 
-const ArticlePage = () => {
+const ArticlePage = async () => {
+
+    // Récupérer les articles
+    const articles = await db.article.findMany({
+        orderBy: { createdAt: 'desc'}
+    }); // db=prisma article=modèle findMany=méthode
+
     return <div>
-        <h1 className="">Blog</h1>
+        <h1 className="mb-4">Blog</h1>
+
+        {/* Liste des articles */}
+        { articles.map((article: any) => (
+            <div key={article.id} className="mb-6">
+                <h2 className="text-2xl font-semibold text-emerald-500">{article.title}</h2>
+                <p>{article.text}</p>
+            </div>
+        )) }
     </div>;
 };
 
