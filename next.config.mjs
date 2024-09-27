e276e441cc39b3c37e102c6e94646226bf165519
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-09-27 17:58:40 +0200

    public key

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+            process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    },
+    reactStrictMode: true,
+};
 
-export default nextConfig;
+module.exports = nextConfig;
